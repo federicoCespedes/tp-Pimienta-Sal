bfcac11a9a6c436584925588191ab4f33f4bf8d5
federicocespedes <fgcespedes1995@gmail.com>
2021-11-24 21:29:38 -0300

    commit

--- controllers/maincontroller.js
+++ controllers/maincontroller.js
@@ -1,11 +1,13 @@
-const platos_db = require('../data/platos.json');
+const platos = require('../data/platos_db')
 
-module.exports = {
-    index : (req,res) => {
-        return res.render('index',{
-            platos : platos_db
-        })
+
+module.exports={
+    index : (req,res)=>{
+        return res.render('index',{platos:platos})
+
     },
-    detail : (req,res) =>   res.render('detalleMenu')
-    
+    detail : (req,res) =>{
+       let plato =platos.find(e => e.id === +req.params.id)
+        res.render('detalleMenu',{plato})
+    }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
-var express = require('express');
-var router = express.Router();
-const {index,detail}= require('../controllers/mainController.js');
-
+const express = require('express');
+const router = express.Router();
+const {index,detail}= require('../controllers/mainController')
 /* GET home page. */
-router.get('/',index);
-router.get('/detalle/:id',detail);
-
+router.get('/', index);
+router.get('/detail/:id', detail);
 
 module.exports = router;
